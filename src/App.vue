6d1e54e953b81f55b32fe981205f8e57f8621eab
etienne <etienne@gmail.com>
2025-09-02 14:43:56 +0200

    création du component menuHeader sur ma branche

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import menuHeader from './components/menuHeader.vue';
+</script>
+
+<template>
+
+  <div>
+    <menuHeader/>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
