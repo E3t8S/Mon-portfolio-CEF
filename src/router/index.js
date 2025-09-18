53cbdbbc63dac9f48ce64662e1b3319f9dbe8a31
etienne <etienne@gmail.com>
2025-09-18 15:52:24 +0200

    commit de la page d'erreur et ajout du router home

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import NotFound from "../views/notFound.vue"
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: ()=> import('@/views/home.vue'),
+
+    },
+    
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/views/notFound.vue')},
+
+  ]
 })
 
 export default router
